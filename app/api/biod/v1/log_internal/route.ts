bc19bdf2ae00c255b3747a6568c5f8a87c2b5d02
Adriansyah <adriansyahazhari28@gmail.com>
2026-01-14 23:11:45 +0700

    add decrypt page

--- app/api/biod/v1/log_internal/route.ts
+++ app/api/biod/v1/log_internal/route.ts
@@ -12,10 +12,10 @@ export async function GET() {
       throw new Error('PREFIX is not defined');
     }
 
-    const full_url = `${baseUrl}/api/biod/v1/log_internal/${prefix}`
+    const full_url = `${baseUrl}/api/biod/v1/log_internal/${prefix}`;
 
-    console.log(full_url)
-    
+    console.log(full_url);
+
     const response = await fetch(full_url, {
       cache: 'no-store',
       headers: {
@@ -32,11 +32,11 @@ export async function GET() {
   } catch (error) {
     console.error('Error fetching data:', error);
     return NextResponse.json(
-      { 
+      {
         status_code: 500,
         message: 'Failed to fetch data from backend service',
         data: [],
-        trace_id: '' 
+        trace_id: '',
       },
       { status: 500 }
     );
